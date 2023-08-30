6bdee8ae70f1b6e1353caf0bcd3bbb3e64c10e2f
rken2010 <rkenshin.2010@gmail.com>
2023-08-30 18:17:27 -0300

    🐞 fix(Fix PokeDex): Repair component

--- src/app/components/PokeDex/PokeDex.tsx
+++ src/app/components/PokeDex/PokeDex.tsx
@@ -6,23 +6,25 @@ import { useState, useEffect } from "react";
 import api from "@/app/api";
 
 
-interface PokeDexProps {
-  pkm: Pokemon;
-  pokeList: Pokemon[];
-}
-
-const PokeDex: React.FC<PokeDexProps> = ({ pkm, pokeList }) => {
+export default function PokeDex({ pkm, pokeList }: { pkm: Pokemon; pokeList: Pokemon[] })   {
+  
 
     const [won, setWon] = useState(false)
-    const [pokemon, setPokemon] = useState(pkm);
-    
+    const [pokemon, setPokemon] = useState<Pokemon>(pkm);
+    const [pokeOption, setPokeoption] = useState<Pokemon[]>(pokeList);
+
     useEffect(() => {
-      api.getPokemon().then(newPkm => {
-        setPokemon(newPkm);
-      });
+      const fetchPokemons = async () => {
+        const pk1 = await api.getPokemon();
+        const pk2 = await api.getPokemon();
+        const pkm = await api.getPokemon();
+        setPokemon(pkm)
+        setPokeoption([pk1, pk2, pkm]);
+      };
+        fetchPokemons();
     }, []);
     
-    function playGame(pokemonSelect:string) {
+    const playGame = (pokemonSelect:string) => {
       let select = pokemonSelect
       let dialog = document.getElementById('dialog-default');
       if (select === pokemon.name) {
@@ -46,20 +48,20 @@ const PokeDex: React.FC<PokeDexProps> = ({ pkm, pokeList }) => {
                   width={512}
                   height={512}
                   style={{imageRendering:"pixelated", filter:"brightness(0)"}}
-                  src={pokemon.image} alt={pokemon.name} />
+                  src={pokemon?.image} alt={pokemon?.name} />
         </div>
         <div className="flex">
-          {pokeList.map((poke)=>(<button onClick={()=>{playGame(poke.name)}} type="submit" value={poke.name} className="nes-btn" key={poke.name}>{poke.name}</button>))}
+          {pokeOption?.map((poke)=>(<button onClick={()=>{playGame(poke?.name)}} type="submit" value={poke?.name} className="nes-btn" key={poke?.name}>{poke?.name}</button>))}
         </div>
         <dialog className="nes-dialog" id="dialog-default">
             <form method="dialog">
             <p className="title">ADIVINA EL POKEMON</p>
-            <p>{won!=true?"Perdiste":"Ganaste"} Es {pokemon.name} </p>
+            <p>{won!=true?"Perdiste":"Ganaste"} Es {pokemon?.name} </p>
             <img 
                   width={512}
                   height={512}
                   style={{imageRendering:"pixelated"}}
-                  src={pokemon.image} alt={pokemon.name} />
+                  src={pokemon?.image} alt={pokemon?.name} />
             <menu className="dialog-menu">
               <button className="nes-btn">Ok</button>
               <button className="nes-btn is-primary" onClick={()=> location.reload()}>Play Again</button>
@@ -70,7 +72,3 @@ const PokeDex: React.FC<PokeDexProps> = ({ pkm, pokeList }) => {
     </div>
   );
 }
-
-export default PokeDex
-
-
