3e27749469229d9241aedc7d040ae3cac91dbfd1
rken2010 <90514981+rken2010@users.noreply.github.com>
2023-08-31 08:55:15 -0300

    add function shuffle in api

--- src/app/api.ts
+++ src/app/api.ts
@@ -18,6 +18,18 @@ const api = {
         }
       );
   },
+  shuffle:(arr) =>{
+	  let i
+    let j
+    let temp
+    for (i = arr.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    return arr;    
+  }
 };
 
 export default api;
